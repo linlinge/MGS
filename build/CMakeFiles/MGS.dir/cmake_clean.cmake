9559266f71581ee910d26747191735b0cc880edd
linlinge <superlinlinge@163.com>
2020-07-17 18:57:44 +0800

    update

---
+++ build/CMakeFiles/MGS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MGS.dir/main.cpp.o"
+  "CMakeFiles/MGS.dir/StyleEvaluation.cpp.o"
+  "CMakeFiles/MGS.dir/HybridMethods.cpp.o"
+  "MGS.pdb"
+  "MGS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MGS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/glib/CMakeFiles/GLIB.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GLIB.dir/BasicGeometry.cpp.o"
+  "CMakeFiles/GLIB.dir/FileExtend.cpp.o"
+  "CMakeFiles/GLIB.dir/Statistics.cpp.o"
+  "CMakeFiles/GLIB.dir/PCLExtend.cpp.o"
+  "CMakeFiles/GLIB.dir/Color.cpp.o"
+  "CMakeFiles/GLIB.dir/Table.cpp.o"
+  "CMakeFiles/GLIB.dir/VectorExtend.cpp.o"
+  "CMakeFiles/GLIB.dir/SignalProcessing.cpp.o"
+  "CMakeFiles/GLIB.dir/StringExtend.cpp.o"
+  "libGLIB.pdb"
+  "libGLIB.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GLIB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
